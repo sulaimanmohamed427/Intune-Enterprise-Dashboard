988041b9f23b75eda428de5c50f449625f84a0e1
Mohamed Sulaiman <sulaimanmohamed427@gmail.com>
2026-06-23 22:45:05 +0400

    Create config.ps1

---
+++ config.ps1
@@ -0,0 +1,6 @@
+$TenantId = "YOUR_TENANT_ID"
+$ClientId = "YOUR_CLIENT_ID"
+$ClientSecret = "YOUR_CLIENT_SECRET"
+
+$Scopes = "https://graph.microsoft.com/.default"
+$OutputPath = ".\output\IntuneReport.html"
