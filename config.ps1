6ffc26eed3707534c832155d838239c2c0ac6b86
Mohamed Sulaiman <sulaimanmohamed427@gmail.com>
2026-06-23 22:57:27 +0400

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,6 +1,6 @@
-$TenantId = "YOUR_TENANT_ID"
-$ClientId = "YOUR_CLIENT_ID"
-$ClientSecret = "YOUR_CLIENT_SECRET"
+$TenantId = "72f988bf-xxxx-xxxx-xxxx"
+$ClientId = "a1b2c3d4-xxxx-xxxx-xxxx"
+$ClientSecret = "your-actual-secret-value"
 
 $Scopes = "https://graph.microsoft.com/.default"
 $OutputPath = ".\output\IntuneReport.html"
